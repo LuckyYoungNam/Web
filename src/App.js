41036e88f100573b378d994e24a76f95f6d597a7
kwon <dohun_dev@naver.com>
2024-11-01 02:41:36 +0900

    .

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import PageRoutes from './routes/pageroutes.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, useNavigate } from 'react-router-dom';
 import useStore from './store/useStore.jsx';
-import PWAInstallPrompt from './PWAInstall.js';
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 function App() {
   const setNavigate = useStore((state) => state.setNavigate);
   const navigate = useNavigate();
-
+  serviceWorkerRegistration.register();
   // Zustand 스토어에 navigate 함수를 전달
   useEffect(() => {
     setNavigate(navigate);
@@ -30,7 +29,6 @@ function App() {
       window.removeEventListener('resize', setScreenSize);
     };
   }, []);
-
   return (
     <div>
       <PageRoutes />
