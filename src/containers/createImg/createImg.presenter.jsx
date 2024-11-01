99936cd625c762cd3eb75ad279e82ab3c70f3a0f
kwon <dohun_dev@naver.com>
2024-11-02 02:08:44 +0900

    test

--- src/containers/createImg/createImg.presenter.jsx
+++ src/containers/createImg/createImg.presenter.jsx
@@ -59,8 +59,8 @@ const CreateImgUI = ({ image, handleImageUpload, region, keywords, handleAddText
                             <S.UploadedImage ref={imageRef} src={image} alt="Uploaded" />
                             {isClick && (
                                 <S.TextOverlay>
-                                    <S.RegionText style={{color:"white", fontSize:"32px"}}>{userData.location}핫플</S.RegionText>
-                                    <S.RegionText regionTextColor={colors.regionTextColor}>{userData.businessName}</S.RegionText>
+                                    <S.RegionText style={{color:"white", fontSize:"32px"}}>{userData.location}서울 핫플</S.RegionText>
+                                    <S.RegionText regionTextColor={colors.regionTextColor}>{userData.businessName}도훈이네김치찌개집이에요</S.RegionText>
                                     <S.KeywordText keywordTextColor={colors.keywordTextColor}>
                                         {keywords.map((keyword, index) => (
                                             <span key={index}>{keyword} </span>
